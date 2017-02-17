062a3cfad48d3b6019462ce441ce660d17e787d2
庞羽 <py008@pangyudeMacBook-Pro.local>
2017-02-17 20:42:05 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,5 +4,5 @@ class Group < ActiveRecord::Base
   validates :title, presence: true
 
   has_many :group_relationships
-  has_manu :members, through: :group_relationships, source: :user
+  has_many :members, through: :group_relationships, source: :user
 end
