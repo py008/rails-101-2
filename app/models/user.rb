a13211ee183822940bae5b1c7180b80c151fc63b
庞羽 <py008@pangyudeMacBook-Pro.local>
2017-02-19 10:41:19 +0800

    implement my posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+         
+  has_many :groups
+  has_many :posts
   has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
 
@@ -18,5 +20,5 @@ class User < ApplicationRecord
   def quit!(group)
     participated_groups.delete(group)
   end
-      
+
 end
